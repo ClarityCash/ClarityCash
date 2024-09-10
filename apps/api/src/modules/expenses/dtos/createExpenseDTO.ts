8951c457b63e5daa479e2bda2053409b696edffd
Juan Ayala <ayalajg@globalhitss.com>
2024-09-10 14:23:39 -0500

    validation fields :zap:

--- apps/api/src/modules/expenses/dtos/createExpenseDTO.ts
+++ apps/api/src/modules/expenses/dtos/createExpenseDTO.ts
@@ -1,8 +1,20 @@
+import { IsString, IsNotEmpty, IsNumber, IsDate, IsPositive, IsOptional } from 'class-validator';
 
 export class createExpenseDTO {
+    @IsString()
+    @IsNotEmpty()
     categoryId: string;
+
+    @IsNumber()
+    @IsPositive()
     amount: number;
+
+    @IsDate()
     date: Date;
+
+    @IsString()
+    @IsOptional()
+    @IsNotEmpty()
     descripcion: string;
 }
 
